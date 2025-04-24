fa1e3a1b58b5c188dbf86079b8e2bed09bbac797
Maurice Ngicho <mrcngicho@gmail.com>
2025-04-24 23:35:58 +0300

    Define Variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that defines and annotates four variables with
+specific types and values.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "ALX"
